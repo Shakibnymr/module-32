405a7ffb76f70e37de79098cf6ac4540dc0a1375
shakibnymr <shakibnymr11@gmail.com>
2023-08-25 05:06:47 +0600

    enough for today!

---
+++ true-false.js
@@ -0,0 +1,33 @@
+/**
+ * Truthy:
+ * 1.true
+ * 2.any number(+,-) will be truthy except 0 
+ * 3.any string is truthy except empty string ''; not this ' '
+ * 4. '0' , 'false'
+ * 5. {}
+ * 6. []
+ * 
+ * Falsy:
+ * 1.false
+ *2. 0 ; false
+ 3. '' (empty string)
+ 4. undefined
+ 5. null
+ * */ 
+
+
+ let x = [];
+ console.log(x);
+if(x){
+    console.log('value of x is truthy');
+}
+else{
+    console.log('value of x is falsy');
+}
+
+// optional
+// check falsy
+const y = null
+if (!y) {
+console.log('value is falsy')
+}
