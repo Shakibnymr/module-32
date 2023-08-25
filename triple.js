0362007980c3fb0041532621db12ac50ff3d167f
shakibnymr <shakibnymr11@gmail.com>
2023-08-25 13:08:18 +0600

    comparison

---
+++ triple.js
@@ -0,0 +1,14 @@
+//always use ===
+//equal comparison doesn't work for non-primitive
+const first =1;
+const second =true;
+if(first == second){
+    console.log('values are equal')
+}
+else {
+    console.log('values ar not equal')
+}
+
+//type coercion
+//type conversion
+//type casting
--- true-false.js
+++ true-false.js
@@ -16,18 +16,24 @@
  * */ 
 
 
- let x = [];
- console.log(x);
-if(x){
-    console.log('value of x is truthy');
-}
-else{
-    console.log('value of x is falsy');
-}
+//  let x = [];
+//  console.log(x);
+// if(x){
+//     console.log('value of x is truthy');
+// }
+// else{
+//     console.log('value of x is falsy');
+// }
 
 // optional
 // check falsy
 const y = null
 if (!y) {
 console.log('value is falsy')
+}
+
+const z = ' ';
+// check truthy
+if(!!z){
+console.log('value is truthy')
 }
